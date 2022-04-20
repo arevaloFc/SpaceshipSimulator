3fd9ca93c57b22c51b188e6fc22f3ad075320807
Fabian Arevalo Roa <arevalo.cpweb@gmail.com>
2022-04-20 18:59:24 +0100

    Feacture: Se creo la funcion llamada mostrarMenu
    Se comprobo que cuando el usuario escoja una opcion saliera correctamente

--- README.md
+++ README.md
@@ -5,40 +5,36 @@
 [![](./imagenes/nave.jpg )](https://wallhere.com/es/wallpaper/1958737)
 1. Cuando el usuario escoja opción de crear una nave espacial el podrá personalizar su nave espacial colocando los
    siguientes atributos:
-    * **Nombre**
-    * **Matricula Galáctica**
-    * **Aceleración**
+   * **Nombre**
+   * **Matricula Galáctica**
+   * **Aceleración**
 
 ![](./imagenes/Linea.jpg)
-
 2. Cuando el usuario coloque su aceleración se podrá calcular la velocidad y la posicion actual de la nave.
 
 ![](./imagenes/Linea.jpg)
 3. El usuario también tendrá una opción donde muestra su nave espacial por medio de AsciiArt
    * [Ejemplos](https://www.asciiart.eu/space/spaceships)
-   
-![](./imagenes/Linea.jpg)
-4. Brake el objetivo de este método es que la nave frene aplicando la misma aceleración pero
-   en sentido contrario.
 
 ![](./imagenes/Linea.jpg)
-5. EmergencyStop el objetivo de este método es que la nave pueda hacer una parada de emergencia (su velocidad quede
-   en 0)
+4. El usuario podrá hacer que la nave frene aplicando la misma aceleración pero en sentido contrario.
+
+![](./imagenes/Linea.jpg)
+5. Tambien podra hacer una parada de emergencia (no se recomienda)
 
 ![](./imagenes/Linea.jpg)
 6. El usuario podrá ver la capacidad de carga máxima que tendrá su nave espacial (el número
    de containers máximos que puede transportar.)
 
 ![](./imagenes/Linea.jpg)
-7. load el objetivo de este método es añadir un container a la bodega de la nave espacial si no hay mas espacio le
-   le saldrá al usuario un mensaje de error
+7. Tambien podra añadir containers a la bodega de la nave espacial, si no hay mas espacio el robot de la nave espacial
+   le avisara con un mensaje
 
 ![](./imagenes/Linea.jpg)
-8. unload  el objetivo de este método es eliminar un container de la bodega de carga de la nave espacial si no hay
-   container en la bodega de la nave le saldrá al usuario un mensaje de error
+8.  Tambien podra quitar containers a la bodega de la nave espacial, si no hay mas containers el robot de la nave              espacial le avisara con un mensaje
 
 ![](./imagenes/Linea.jpg)
-9. El usuario tendrá una opción para salir del programa
+9. El usuario tendrá una opción de salir de la nave espacial (mala idea)
 
 ![](./imagenes/Universo.jpg)
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scan = new Scanner(System.in);
+
+    /*------------------------------------------------Menu--------------------------------------------*/
+    public static String mostrarMenu() {
+
+        System.out.println(". . . . . . Menu . . . . . .");
+        System.out.println("1. Crear nave espacial.");
+        System.out.println("2. Mostrar nave espacial.");
+        System.out.println("3. Acelerar nave espacial");
+        System.out.println("4. Frenar nave espacial.");
+        System.out.println("5. Parar nave espacial");
+        System.out.println("6. Agregar containers a la bodega de la nave espacial");
+        System.out.println("7. Quitar containers a la bodega de la nave espacial");
+        System.out.println("0 → Salir de la nave.");
+        System.out.println("Hola capitan por favor elige una opción del menu anteriormente mostrado");
+
+        String opcionUsuario = scan.nextLine();
+        return opcionUsuario;
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        String opcionUsuario = "";
+
+        while (!opcionUsuario.equals("0")) {
+            opcionUsuario = mostrarMenu();
+
+            switch (opcionUsuario) {
+                case "1":
+                    System.out.println("Hola1");
+                    break;
+                case "2":
+                    System.out.println("Hola2");
+                    break;
+                case "3":
+                    System.out.println("Hola3");
+                    break;
+                case "4":
+                    System.out.println("Hola4");
+                    break;
+                case "5":
+                    System.out.println("Hola5");
+                    break;
+                case "6":
+                    System.out.println("Hola6");
+                    break;
+                case "7":
+                    System.out.println("Hola7");
+                    break;
+                case "0":
+                    System.out.println("Estas Muerto :D");
+                    break;
+                default:
+                    System.out.println("Por favor escoja una opcion de 1 al 7 o 0 para salir gracias");
+                    break;
+            }
+        }
     }
+
 }
